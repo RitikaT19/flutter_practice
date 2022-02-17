1a3e8677d3a8d6c1394e754a2ef1ab00b8dbefe3
RitikaT19 <ritika.tiwari@headstrait.com>
2022-02-17 11:30:35 +0530

    Added: Theme, text and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import "package:flutter/material.dart";
-import 'package:flutter_practice/home_page.dart';
+import 'package:flutter_practice/pages/home_page.dart';
+import 'package:flutter_practice/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
-  
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:HomePage()
+      // home: HomePage(),
+      themeMode: ThemeMode.light, //for light bg
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark), //for dark bg
+      routes: {
+        "/": (context) => LoginPage(), //route for home page
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
