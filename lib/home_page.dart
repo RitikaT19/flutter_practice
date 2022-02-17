169e462c48969c387e16d74ec4b4238276aa351c
RitikaT19 <ritika.tiwari@headstrait.com>
2022-02-17 10:42:11 +0530

    Added: First commit

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatelessWidget {
+  @override
+  final int days = 30;
+  final String name = "Demo";
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello"),
+        ),
+      ),
+    );
+  }
+}
