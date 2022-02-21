98f5c9c4e91454ed27aaf0c9eb6035f42fc90686
RitikaT19 <ritika.tiwari@headstrait.com>
2022-02-21 11:41:50 +0530

    Added: Googlr fonts, image and button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:flutter_practice/pages/home_page.dart';
 import 'package:flutter_practice/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light, //for light bg
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark), //for dark bg
       routes: {
         "/": (context) => LoginPage(), //route for home page
