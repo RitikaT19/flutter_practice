12eaf1c6e3451546e38a5d92c1a9cfab464b893f
RitikaT19 <ritika.tiwari@headstrait.com>
2022-02-21 12:06:55 +0530

    Added: SingleChildScrollView and navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:flutter_practice/pages/home_page.dart';
 import 'package:flutter_practice/pages/login_page.dart';
+import 'package:flutter_practice/pages/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -17,10 +18,12 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.lato().fontFamily,
       ),
+      debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(brightness: Brightness.dark), //for dark bg
       routes: {
-        "/": (context) => LoginPage(), //route for home page
-        "/login": (context) => LoginPage()
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute:((context) => HomePage()), //route for home page
+        MyRoutes.loginRoute:((context) => LoginPage())
       },
     );
   }
